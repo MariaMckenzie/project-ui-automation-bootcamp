b51ff3539bc12c0ab0ac4eafeeafe5cafc777a66
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-12-07 12:18:56 -0500

    Create cypress.config.js

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+    chromeWebSecurity: false,
+    e2e: {
+        setupNodeEvents(on, config) {
+        // implement node event listeners here
+        },
+        baseUrl: "https://www.saucedemo.com",
+        specPattern: 'cypress/e2e/tests/**/*.cy.{js,jsx,ts,tsx}',
+        experimentalSessionAndOrigin: true,
+        chromeWebSecurity: false,
+        reporter: 'mochawesome',
+        reporterOptions: {
+            reportDir: 'cypress/reports',
+            overwrite: false,
+            html: false,
+            json: true
+        }
+    },
+});
