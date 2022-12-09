0fefe98c78bf3d9115c7a7df3fdbfe7b2789245c
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-12-09 02:06:11 -0500

    Update add-to-cart.cy.js

--- cypress/e2e/tests/add-to-cart/add-to-cart.cy.js
+++ cypress/e2e/tests/add-to-cart/add-to-cart.cy.js
@@ -1,5 +1,5 @@
-import { existingUser, invalidData, missingData, missingEmail, missingPassword, newUser, nonexistingUser, validUser } from "../../data/product.data"
-import authenticationPage from "../../pages/details.page"
+import { existingUser } from "../../data/user.data"
+import authenticationPage from "../../pages/authentication.page"
 import homePage from "../../pages/home.page"
 
 
@@ -13,12 +13,11 @@ describe('Authentication', () => {
      * Test Case ID: E2E_9
      * Test Scenario: Check add-to-cart functionality
      */
-    it('should verify that the user cannot sign up if one or more input fields are missing', () => {
+    it('should verify that the user can add an item to the cart from the ‘products’ page', () => {
         cy.get(authenticationPage.signInOrRegisterButton).click()
 
-        // wait for the page to load and select the sign up section
+        // wait for the page to load
         cy.wait(1000)
-        cy.get(authenticationPage.signupSection).click()
 
         // attempt to sign in with valid user data
         authenticationPage.loginOrSignup(existingUser.email, existingUser.password)
