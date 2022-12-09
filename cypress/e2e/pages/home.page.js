c85e95ed3f6d169b70cf2b67815ce93c20846196
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-12-09 01:52:07 -0500

    added and removed pages

--- cypress/e2e/pages/home.page.js
+++ cypress/e2e/pages/home.page.js
@@ -22,5 +22,72 @@ class Home {
      */
     get contactButton () { return ("#top-contact") }
 
+
+    /**
+     * @returns the title of the products section of the page
+     */
+    get title () { return (".chakra-heading.css-1jhlc8u") }
+
+
+    /**
+     * @returns the first product card
+     */
+    get firstProductCard () { return ("#product-0") }
+
+
+    /**
+     * @returns the input field for the product quantity
+     */
+    get productQuantity () { return (".css-a8qclj") }
+
+
+    /**
+     * @returns the product name
+     */
+    get productName () { return (".css-1n64n71j") }
+
+
+    /**
+     * @returns the product price
+     */
+    get productPrice () { return (".css-0") }
+
+
+
+    // METHODS
+
+    /**
+     * Retruns the nth product card 
+     * **(Note: The maximum number of products is 22)**
+     * @param {Number} itemNo nth item in the list
+     * @returns the nth product card
+     */
+    getProductCard (itemNo) {
+        return (`#product-${itemNo-1}`)
+    }
+
+
+    /**
+     * Returns the name and price of the nth product card 
+     * **(Note: The maximum number of products is 22)**
+     * @param {Number} itemNo nth item in the list
+     * @returns a list containing the product name and price
+     */
+    getProductCardData (itemNo) {
+        return ([`#product-${itemNo-1} ${this.productName}`, `#product-${itemNo-1} ${this.productPrice}`])
+    }
+
+
+    /**
+     * Sets the quantity of the product
+     * **(Note: The maximum number of products is 22)**
+     * @param {Number} itemNo nth item in the list
+     * @param {Number} quantity number of products required
+     * @returns a list containing the product name and price
+     */
+    getProductCardData (itemNo, quantity) {
+        cy.get(`#product-${itemNo-1} ${this.productQuantity}`).type(quantity)
+    }
+
 }
 export default new Home()
