6451c512fc1848f34128e1b3fe65965437414c18
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-12-10 08:55:26 -0500

    Update cart.cy.js

--- cypress/e2e/tests/cart/cart.cy.js
+++ cypress/e2e/tests/cart/cart.cy.js
@@ -10,7 +10,7 @@ describe('Cart', () => {
     // variables
     let total, totalQuantity
 
-    before(() => {
+    beforeEach(() => {
         cy.clearCookies() // clear all cookies before starting the tests
         cy.visit('/')
 
@@ -46,15 +46,15 @@ describe('Cart', () => {
 
         // product 1
         cy.get(cartPage.getProductCardData(3)[0]).should("contain", product1.name) // check product name
-        cy.get(cartPage.getProductCardData(3)[1]).should("contain", `$${product1.price}.00`) // check product price
+        cy.get(cartPage.getProductCardData(3)[1]).should("contain", `$${product1.price * product1.quantity}.00`) // check product price
         cy.get(cartPage.getProductCardData(3)[2]).should("have.text", `${product1.quantity}`) // check product quantity
         // product 2
         cy.get(cartPage.getProductCardData(2)[0]).should("contain", product4.name) // check product name
-        cy.get(cartPage.getProductCardData(2)[1]).should("contain", `$${product4.price}.00`) // check product price
+        cy.get(cartPage.getProductCardData(2)[1]).should("contain", `$${product4.price * product4.quantity}.00`) // check product price
         cy.get(cartPage.getProductCardData(2)[2]).should("have.text", `${product4.quantity}`) // check product quantity
         // product 3
         cy.get(cartPage.getProductCardData(1)[0]).should("contain", product6.name) // check product name
-        cy.get(cartPage.getProductCardData(1)[1]).should("contain", `$${product6.price}.00`) // check product price
+        cy.get(cartPage.getProductCardData(1)[1]).should("contain", `$${product6.price * product6.quantity}.00`) // check product price
         cy.get(cartPage.getProductCardData(1)[2]).should("have.text", `${product6.quantity}`) // check product quantity
     })
     
@@ -62,16 +62,24 @@ describe('Cart', () => {
      * Test Case ID: E2E_
      * Test Scenario: Check cart functionality
      */
-    it('should verify that the user cannot modify the price or subtotal of the product', () => {
+    it.only('should verify that the user cannot modify the price or subtotal of the product', () => {
+        // add multiple products to cart
+        homePage.addToCart(1, product1.quantity)
+        cy.wait(500)
+        cartPage.closeCart()
+        homePage.addToCart(4, product4.quantity)
+        cy.wait(500)
+        cartPage.closeCart()
+        homePage.addToCart(6, product6.quantity)
+
         // calculate total
         total = (product1.price * product1.quantity) + (product4.price * product4.quantity) + (product6.price * product6.quantity)
 
         // check for correct subtotal
         cy.get(cartPage.cartTotal).should("contain", `$${total}.00`)
 
-        // assert that chnages cannot be made
-        cy.get(cartPage.getProductCardData(2)[1]).should("not.be.clickable").should("not.change")
-        cy.get(cartPage.subtotal).should("not.be.clickable").should("not.change")
+        // assert that changes cannot be made
+        cy.get(cartPage.cartTotal).should("")
 
     })
     
@@ -80,6 +88,15 @@ describe('Cart', () => {
      * Test Scenario: Check cart functionality
      */
     it('should verify that the total number of items added to the cart is consistent with the product quantities', () => {
+        // add multiple products to cart
+        homePage.addToCart(1, product1.quantity)
+        cy.wait(500)
+        cartPage.closeCart()
+        homePage.addToCart(4, product4.quantity)
+        cy.wait(500)
+        cartPage.closeCart()
+        homePage.addToCart(6, product6.quantity)
+
         // calculate total quantity
         totalQuantity = product1.quantity + product4.quantity + product6.quantity    
 
@@ -93,6 +110,15 @@ describe('Cart', () => {
      * Test Scenario: Check cart functionality
      */
     it('should verify that the user can modify the quantity of a product in the cart', () => {
+        // add multiple products to cart
+        homePage.addToCart(1, product1.quantity)
+        cy.wait(500)
+        cartPage.closeCart()
+        homePage.addToCart(4, product4.quantity)
+        cy.wait(500)
+        cartPage.closeCart()
+        homePage.addToCart(6, product6.quantity)
+
         // select one item and modify the quanitity
         cartPage.incrementProductQuantity(2)
         cartPage.decrementProductQuantity(1)
@@ -107,7 +133,16 @@ describe('Cart', () => {
      * Test Case ID: E2E_
      * Test Scenario: Check cart functionality
      */
-    it('should verify that the user can remove items from cart', () => {
+    it('should verify that the user can remove items from cart', () => {        
+        // add multiple products to cart
+        homePage.addToCart(1, product1.quantity)
+        cy.wait(500)
+        cartPage.closeCart()
+        homePage.addToCart(4, product4.quantity)
+        cy.wait(500)
+        cartPage.closeCart()
+        homePage.addToCart(6, product6.quantity)
+
         // remove item from cart
         cartPage.removeProductFromCart(3) // remove product1
 
