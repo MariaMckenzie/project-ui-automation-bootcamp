136f48815c715c073e078eff7a2bfbc5800e3b39
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-12-12 07:08:40 -0500

    Update product.data.js

--- cypress/e2e/data/product.data.js
+++ cypress/e2e/data/product.data.js
@@ -3,132 +3,154 @@ module.exports = {
         name: "Quality Fitted Hat", 
         quantity: 1, 
         price: 30, 
-        desc: "3 Pack Unisex Vintage Washed Distressed Baseball-Cap,Retro Adjustable Dad Hats,Baseball Hat for Men Women"
+        desc: "3 Pack Unisex Vintage Washed Distressed Baseball-Cap,Retro Adjustable Dad Hats,Baseball Hat for Men Women",
+        category: "hat"
     },
     product2: { 
         name: "Quality Trucker Hat", 
         quantity: 1, 
         price: 24, 
-        desc: ""
+        desc: "",
+        category: "hat"
     },
     product3: { 
         name: "Quality Mousepad", 
         quantity: 2, 
         price: 20, 
-        desc: ""
+        desc: "",
+        category: "computer"
     },
     product4: { 
         name: "Quality Mug", 
         quantity: 3, 
         price: 15, 
-        desc: "Wampumtuk Pack of 2 Company Policies Boss Employee Work Office 11 Ounces Funny Coffee Mug"
+        desc: "Wampumtuk Pack of 2 Company Policies Boss Employee Work Office 11 Ounces Funny Coffee Mug",
+        category: "cup"
     },
     product5: { 
         name: "Quality Pillow", 
         quantity: 1, 
         price: 20, 
-        desc: ""
+        desc: "",
+        category: "pillow"
     },
     product6: { 
         name: "Quality Hooded Sweatshirt", 
-        quantity: 1, 
+        quantity: 5, 
         price: 50, 
-        desc: "Hanes Men's Sweatshirt, EcoSmart Fleece Crewneck Sweatshirt, Cotton-Blend Fleece Sweatshirt, Plush Fleece Pullover Sweatshirt"
+        desc: "Hanes Men's Sweatshirt, EcoSmart Fleece Crewneck Sweatshirt, Cotton-Blend Fleece Sweatshirt, Plush Fleece Pullover Sweatshirt",
+        category: "shirt"
     },
     product7: { 
         name: "Quality Sweatshirt", 
         quantity: 2, 
         price: 40, 
-        desc: ""
+        desc: "",
+        category: "shirt"
     },
     product8: { 
         name: "Quality Kids Tshirt", 
         quantity: 2, 
         price: 15, 
-        desc: ""
+        desc: "",
+        category: "shirt"
     },
     product9: { 
         name: "Quality Tshirt", 
         quantity: 1, 
         price: 20, 
-        desc: ""
+        desc: "",
+        category: "shirt"
     },
     product10: { 
         name: "Quality Vneck", 
         quantity: 2, 
         price: 20, 
-        desc: ""
+        desc: "",
+        category: "shirt"
     },
     product11: { 
         name: "Quality Pink Pants", 
         quantity: 1, 
         price: 44, 
-        desc: ""
+        desc: "",
+        category: "pants"
     },
     product12: { 
         name: "Quality Cargo Pants", 
         quantity: 1, 
         price: 17, 
-        desc: ""
+        desc: "",
+        category: "pants"
     },
     product13: { 
         name: "Quality Jeans Pants", 
-        quantity: "", 
+        quantity: 2, 
         price: 28, 
-        desc: ""
+        desc: "",
+        category: "pants"
     },
     product14: { 
         name: "Quality Blue Shoes", 
         quantity: 1, 
         price: 20, 
-        desc: ""
+        desc: "",
+        category: "shoes"
     },
     product15: { 
         name: "Quality Stylish Shoes", 
         quantity: 1, 
         price: 20, 
-        desc: ""
+        desc: "",
+        category: "shoes"
     },
     product16: { 
         name: "Quality Heal Shoes", 
         quantity: 1, 
         price: 20, 
-        desc: ""
+        desc: "",
+        category: "shoes"
     },
     product17: { 
         name: "Red Couch", 
         quantity: 1, 
         price: 320, 
-        desc: ""
+        desc: "",
+        category: "couch"
     },
     product18: { 
         name: "White Couch", 
         quantity: 1, 
         price: 234, 
-        desc: ""
+        desc: "",
+        category: "couch"
     },
     product19: { 
         name: "Gray Couch", 
-        quantity: 1, 
+        quantity: 5, 
         price: 550, 
-        desc: ""
+        desc: "",
+        category: "couch"
     },
     product20: { 
         name: "Mackbook Pro", 
         quantity: 1, 
         price: 2000, 
-        desc: ""
+        desc: "",
+        category: "laptop"
     },
     product21: { 
         name: "Dell Laptop", 
         quantity: 1, 
         price: 1000, 
-        desc: ""
+        desc: "",
+        category: "laptop"
     },
     product22: { 
         name: "HP Laptop", 
         quantity: 1, 
         price: 680, 
-        desc: ""
+        desc: "",
+        category: "laptop"
     },
 }
