30a60111a1e97b72ed7c500867cc0c984e7bde64
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-12-12 10:07:10 -0500

    Update page

--- cypress/e2e/pages/cart.page.js
+++ cypress/e2e/pages/cart.page.js
@@ -1,60 +1,54 @@
 
 /**
-* Contains the locators and selectors found on the Cart page. 
+* Contains the locators and selectors found on the cart page. 
 */
 
 class Cart {
 
     /**
-     * @returns the title of the cart
+     * @returns the selector for the title of the cart
      */
     get title () { return (".snipcart-cart-header__title") }
 
 
     /**
-     * @returns the first product card (the last product added)
+     * @returns the selector for the first product card (the last product added)
      */
     get firstProductCard () { return (".snipcart-item-list.snipcart-scrollbar.snipcart-item-list--no-shadow > li:nth-child(1) > div > div") }
 
 
     /**
-     * @returns the input field for the product quantity
+     * @returns the selector for the product list
      */
     get productsList () { return (".snipcart-item-list.snipcart-scrollbar.snipcart-item-list--no-shadow") }
 
 
     /**
-     * @returns the product name
+     * @returns the selector for the product name
      */
     get productName () { return (".snipcart-item-line__title") }
 
 
     /**
-     * @returns the product price
+     * @returns the selector for the product price
      */
     get productPrice () { return (".snipcart-item-quantity__total-price") }
 
 
     /**
-     * @returns the product quantity
+     * @returns the selector for the product quantity
      */
     get productQuantity () { return (".snipcart-item-quantity__quantity") }
 
 
     /**
-     * @returns the empty cart error
-     */
-    get emptyCartError () { return (".snipcart-empty-cart__title") }
-
-
-    /**
-     * @returns the button that takes the user back to the cart
+     * @returns the selector for the button that closes the cart
      */
     get backToHomeButton () { return (".snipcart-modal__close-title") }
 
 
     /**
-     * @returns the subtotal of all products in the cart
+     * @returns the selector for the subtotal of all products in the cart
      */
     get cartTotal () { return (".snipcart-summary-fees__amount") }
 
@@ -71,20 +65,20 @@ class Cart {
     
     
     /**
-     * Retruns the nth product card 
+     * Returns the nth product card 
      * **(Note: The maximum number of products is 22)**
      * @param {Number} itemNo nth item in the list
      * @returns the nth product card
      */
     getProductCard (itemNo) {
-        return (`${itemNo}`)
+        return (`${itemNo}`)  //TO BE FIXED
     }
 
 
     /**
-     * Returns the name and price of the nth product  
+     * Returns the name, price and quantity of the nth product  
      * @param {Number} itemNo nth item in the list
-     * @returns a list containing the product name, price and quantity
+     * @returns a list of selectors for the product name, price and quantity
      */
     getProductCardData (itemNo) {
         return (
@@ -92,18 +86,6 @@ class Cart {
                 `${this.productsList} > li:nth-child(${itemNo}) > div > div ${this.productPrice}`,
                 `${this.productsList} > li:nth-child(${itemNo}) > div > div ${this.productQuantity}`
             ])
-    }
-
-    /**
-     * Adds commas to large numbers to seperate it in thousands
-     * @param {*} num a number
-     * @returns 
-     */
-    numberWithCommas(num) {
-        if (num > 1000) {
-            return num.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
-        }
-        return num
     }
 
 }
