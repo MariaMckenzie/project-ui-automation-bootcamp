3769b7eede0f8e1f828163f95d5dbdab3d895f62
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-12-13 01:12:33 -0500

    Update search.cy.js

--- cypress/e2e/tests/search/search.cy.js
+++ cypress/e2e/tests/search/search.cy.js
@@ -1,4 +1,3 @@
-import { product1, product2, product3, product4, product5, product6, product7, product8, product9, product10, product11, product12, product13, product14, product15, product16, product17, product18, product19, product20, product21, product22 } from "../../data/product.data"
 import { existingUser } from "../../data/user.data"
 import authenticationPage from "../../pages/authentication.page"
 import homePage from "../../pages/home.page"
