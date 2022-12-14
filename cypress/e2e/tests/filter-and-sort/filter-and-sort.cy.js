084685ce190f54e540ce5040c40f4dee2fccca35
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-12-14 18:49:42 -0500

    Update filter-and-sort.cy.js

--- cypress/e2e/tests/filter-and-sort/filter-and-sort.cy.js
+++ cypress/e2e/tests/filter-and-sort/filter-and-sort.cy.js
@@ -3,34 +3,23 @@ import { existingUser } from "../../data/user.data"
 import authenticationPage from "../../pages/authentication.page"
 import homePage from "../../pages/home.page"
 
-describe('Sort-and-Filter', () => {
-    // variables
-    let lst, key, category
-    let productsList = [
-        [product1.name, product1.category, product1.price],
-        [product2.name, product2.category, product2.price],
-        [product3.name, product3.category, product3.price],
-        [product4.name, product4.category, product4.price],
-        [product5.name, product5.category, product5.price],
-        [product6.name, product6.category, product6.price],
-        [product7.name, product7.category, product7.price],
-        [product8.name, product8.category, product8.price],
-        [product9.name, product9.category, product9.price],
-        [product10.name, product10.category, product10.price],
-        [product11.name, product11.category, product11.price],
-        [product12.name, product12.category, product12.price],
-        [product13.name, product13.category, product13.price],
-        [product14.name, product14.category, product14.price],
-        [product15.name, product15.category, product15.price],
-        [product16.name, product16.category, product16.price],
-        [product17.name, product17.category, product17.price],
-        [product18.name, product18.category, product18.price],
-        [product19.name, product19.category, product19.price],
-        [product20.name, product20.category, product20.price],
-        [product21.name, product21.category, product21.price],
-        [product22.name, product22.category, product22.price]
-    ]
-
+describe('Sort-and-Filter', () => {    
+        // variables
+        let lst, key, category
+        let productsList = [
+            [product1.name, product1.category, product1.price], [product2.name, product2.category, product2.price],
+            [product3.name, product3.category, product3.price], [product4.name, product4.category, product4.price],
+            [product5.name, product5.category, product5.price], [product6.name, product6.category, product6.price],
+            [product7.name, product7.category, product7.price], [product8.name, product8.category, product8.price],
+            [product9.name, product9.category, product9.price], [product10.name, product10.category, product10.price],
+            [product11.name, product11.category, product11.price], [product12.name, product12.category, product12.price],
+            [product13.name, product13.category, product13.price], [product14.name, product14.category, product14.price],
+            [product15.name, product15.category, product15.price], [product16.name, product16.category, product16.price],
+            [product17.name, product17.category, product17.price], [product18.name, product18.category, product18.price],
+            [product19.name, product19.category, product19.price], [product20.name, product20.category, product20.price],
+            [product21.name, product21.category, product21.price], [product22.name, product22.category, product22.price]
+        ]
+    
     beforeEach(() => {
         cy.clearCookies() // clear all cookies before starting the tests
         cy.visit('/')
@@ -49,8 +38,6 @@ describe('Sort-and-Filter', () => {
     })
 
 
-
-
     /**
      * Test Case ID: E2E_54
      * Test Scenario: Check the functionality of the sort-and-filter
@@ -58,7 +45,7 @@ describe('Sort-and-Filter', () => {
     it("should verify that the sort and filter selections are visible", () => {
         cy.get(homePage.sortSelection)
             .should("be.visible")
-        cy.get(homePage.filterProducts)
+        cy.get(homePage.categorySelection)
             .should("be.visible")
     })
     
@@ -69,19 +56,19 @@ describe('Sort-and-Filter', () => {
     it("should verify that the user can sort the products by name in ascending order", () => {
         // sort products by name (a-z)
         homePage.sortProducts('az')
-        productsList.sort(function(a, b){ return a[0].localeCompare(b[0]) }) // sort products list
+        lst = productsList.sort(function(a, b){ return a[0].localeCompare(b[0]) }) // sort products list
 
         // assert that the products are sorted 
         cy.get(homePage.getProductCardData(1)[0])
-            .should("have.text", productsList[0][0])
+            .should("have.text", lst[0][0])
         cy.get(homePage.getProductCardData(2)[0])
-            .should("have.text", productsList[1][0])
+            .should("have.text", lst[1][0])
         cy.get(homePage.getProductCardData(11)[0])
-            .should("have.text", productsList[10][0])
+            .should("have.text", lst[10][0])
         cy.get(homePage.getProductCardData(21)[0])
-            .should("have.text", productsList[20][0])
+            .should("have.text", lst[20][0])
         cy.get(homePage.getProductCardData(22)[0])
-            .should("have.text", productsList[21][0])
+            .should("have.text", lst[21][0])
     })
     
     /**
@@ -91,19 +78,19 @@ describe('Sort-and-Filter', () => {
     it("should verify that the user can sort the products by name in descending order", () => {
         // sort products by name (a-z)
         homePage.sortProducts('za')
-        productsList.sort(function(a, b){ return b[0].localeCompare(a[0]) }) // sort products list
+        lst = productsList.sort(function(a, b){ return b[0].localeCompare(a[0]) }) // sort products list
 
         // assert that the products are sorted 
         cy.get(homePage.getProductCardData(1)[0])
-            .should("have.text", productsList[0][0])
+            .should("have.text", lst[0][0])
         cy.get(homePage.getProductCardData(2)[0])
-            .should("have.text", productsList[1][0])
+            .should("have.text", lst[1][0])
         cy.get(homePage.getProductCardData(11)[0])
-            .should("have.text", productsList[10][0])
+            .should("have.text", lst[10][0])
         cy.get(homePage.getProductCardData(21)[0])
-            .should("have.text", productsList[20][0])
+            .should("have.text", lst[20][0])
         cy.get(homePage.getProductCardData(22)[0])
-            .should("have.text", productsList[21][0])
+            .should("have.text", lst[21][0])
     })
     
     /**
@@ -113,9 +100,9 @@ describe('Sort-and-Filter', () => {
     it("should verify that the user can sort the products by price in ascending order", () => {
         // sort products by price (low - high)
         homePage.sortProducts('loHi')
-        productsList.sort(function(a, b){ 
+        lst = productsList.sort(function(a, b){ 
             if (a[2] === b[2]) {
-                return a[0].localeCompare(b[0])
+                return b[0].localeCompare(a[0])
             } return a[2] - b[2]             
         }) // sort products list
 
@@ -123,15 +110,15 @@ describe('Sort-and-Filter', () => {
 
         // assert that the products are sorted 
         cy.get(homePage.getProductCardData(1)[0])
-            .should("have.text", productsList[0][0])
+            .should("have.text", lst[0][0])
         cy.get(homePage.getProductCardData(2)[0])
-            .should("have.text", productsList[1][0])
+            .should("have.text", lst[1][0])
         cy.get(homePage.getProductCardData(11)[0])
-            .should("have.text", productsList[10][0])
+            .should("have.text", lst[10][0])
         cy.get(homePage.getProductCardData(21)[0])
-            .should("have.text", productsList[20][0])
+            .should("have.text", lst[20][0])
         cy.get(homePage.getProductCardData(22)[0])
-            .should("have.text", productsList[21][0])
+            .should("have.text", lst[21][0])
     })
     
     /**
@@ -141,30 +128,30 @@ describe('Sort-and-Filter', () => {
     it("should verify that the user can sort the products by price in descending order", () => {
         // sort products by price (high - low)
         homePage.sortProducts('hiLo')
-        productsList.sort(function(a, b){ 
+        lst = productsList.sort(function(a, b){ 
             if (a[2] === b[2]) {
-                return a[0].localeCompare(b[0])
+                return b[0].localeCompare(a[0])
             } return b[2] - a[2]             
         }) // sort products list
 
         // assert that the products are sorted 
         cy.get(homePage.getProductCardData(1)[0])
-            .should("have.text", productsList[0][0])
+            .should("have.text", lst[0][0])
         cy.get(homePage.getProductCardData(2)[0])
-            .should("have.text", productsList[1][0])
+            .should("have.text", lst[1][0])
         cy.get(homePage.getProductCardData(11)[0])
-            .should("have.text", productsList[10][0])
+            .should("have.text", lst[10][0])
         cy.get(homePage.getProductCardData(21)[0])
-            .should("have.text", productsList[20][0])
+            .should("have.text", lst[20][0])
         cy.get(homePage.getProductCardData(22)[0])
-            .should("have.text", productsList[21][0])
+            .should("have.text", lst[21][0])
     })    
     
     /**
      * Test Case ID: E2E_59
      * Test Scenario: Check the functionality of the sort-and-filter
      */
-    it("should verify that the user can sort the search results on the ‘product’ page", () => {
+    it("should verify that the user can sort the search results on the home page", () => {
         // search for product
         key = "shirt"
         homePage.search(key)
@@ -188,6 +175,22 @@ describe('Sort-and-Filter', () => {
      * Test Scenario: Check the functionality of the sort-and-filter
      */
     it("should verify that the user can filter products by category", () => {
+        // clear list
+        lst = []
+        productsList = [
+            [product1.name, product1.category, product1.price], [product2.name, product2.category, product2.price],
+            [product3.name, product3.category, product3.price], [product4.name, product4.category, product4.price],
+            [product5.name, product5.category, product5.price], [product6.name, product6.category, product6.price],
+            [product7.name, product7.category, product7.price], [product8.name, product8.category, product8.price],
+            [product9.name, product9.category, product9.price], [product10.name, product10.category, product10.price],
+            [product11.name, product11.category, product11.price], [product12.name, product12.category, product12.price],
+            [product13.name, product13.category, product13.price], [product14.name, product14.category, product14.price],
+            [product15.name, product15.category, product15.price], [product16.name, product16.category, product16.price],
+            [product17.name, product17.category, product17.price], [product18.name, product18.category, product18.price],
+            [product19.name, product19.category, product19.price], [product20.name, product20.category, product20.price],
+            [product21.name, product21.category, product21.price], [product22.name, product22.category, product22.price]
+        ]
+
         // filter products by category
         category = "hat"
         lst = productsList.filter(function(x) {return x[1].includes(category)})
@@ -202,7 +205,7 @@ describe('Sort-and-Filter', () => {
         
             
         // filter products by category
-        category = "pant"
+        category = "shirt"
         lst = productsList.filter(function(x) {return x[1].includes(category)})
 
         homePage.filterProducts(category)
@@ -236,8 +239,24 @@ describe('Sort-and-Filter', () => {
      * Test Scenario: Check the functionality of the sort-and-filter
      */
     it("should verify that the user can sort the filtered products", () => {
+        // clear list
+        lst = []
+        productsList = [
+            [product1.name, product1.category, product1.price], [product2.name, product2.category, product2.price],
+            [product3.name, product3.category, product3.price], [product4.name, product4.category, product4.price],
+            [product5.name, product5.category, product5.price], [product6.name, product6.category, product6.price],
+            [product7.name, product7.category, product7.price], [product8.name, product8.category, product8.price],
+            [product9.name, product9.category, product9.price], [product10.name, product10.category, product10.price],
+            [product11.name, product11.category, product11.price], [product12.name, product12.category, product12.price],
+            [product13.name, product13.category, product13.price], [product14.name, product14.category, product14.price],
+            [product15.name, product15.category, product15.price], [product16.name, product16.category, product16.price],
+            [product17.name, product17.category, product17.price], [product18.name, product18.category, product18.price],
+            [product19.name, product19.category, product19.price], [product20.name, product20.category, product20.price],
+            [product21.name, product21.category, product21.price], [product22.name, product22.category, product22.price]
+        ]
+
          // filter products by category
-        category = "pant"
+        category = "couch"
         lst = productsList.filter(function(x) {return x[1].includes(category)})
 
         homePage.filterProducts(category)
@@ -254,7 +273,7 @@ describe('Sort-and-Filter', () => {
         homePage.sortProducts('loHi')
         lst = lst.sort(function(a, b){ 
             if (a[2] === b[2]) {
-                return a[0].localeCompare(b[0])
+                return b[0].localeCompare(a[0])
             } return a[2] - b[2]             
         }) // sort products list
 
@@ -271,7 +290,23 @@ describe('Sort-and-Filter', () => {
      * Test Case ID: E2E_62
      * Test Scenario: Check the functionality of the sort-and-filter
      */
-    it("should verify that the user can filter the search results on the ‘product’ page", () => {
+    it("should verify that the user can filter the search results on the home page", () => {
+        // clear list
+        lst = []
+        productsList = [
+            [product1.name, product1.category, product1.price], [product2.name, product2.category, product2.price],
+            [product3.name, product3.category, product3.price], [product4.name, product4.category, product4.price],
+            [product5.name, product5.category, product5.price], [product6.name, product6.category, product6.price],
+            [product7.name, product7.category, product7.price], [product8.name, product8.category, product8.price],
+            [product9.name, product9.category, product9.price], [product10.name, product10.category, product10.price],
+            [product11.name, product11.category, product11.price], [product12.name, product12.category, product12.price],
+            [product13.name, product13.category, product13.price], [product14.name, product14.category, product14.price],
+            [product15.name, product15.category, product15.price], [product16.name, product16.category, product16.price],
+            [product17.name, product17.category, product17.price], [product18.name, product18.category, product18.price],
+            [product19.name, product19.category, product19.price], [product20.name, product20.category, product20.price],
+            [product21.name, product21.category, product21.price], [product22.name, product22.category, product22.price]
+        ]
+
         // search for product
         key = "c"
         category = "couch"
@@ -294,9 +329,25 @@ describe('Sort-and-Filter', () => {
      * Test Case ID: E2E_63
      * Test Scenario: Check the functionality of the sort-and-filter
      */
-    it("should verify that the user can reset the selected sort and filter options", () => {
+    it("should verify that the user can reset the selected sort and filter options", () => {        
+        // clear list
+        lst = []
+        productsList = [
+            [product1.name, product1.category, product1.price], [product2.name, product2.category, product2.price],
+            [product3.name, product3.category, product3.price], [product4.name, product4.category, product4.price],
+            [product5.name, product5.category, product5.price], [product6.name, product6.category, product6.price],
+            [product7.name, product7.category, product7.price], [product8.name, product8.category, product8.price],
+            [product9.name, product9.category, product9.price], [product10.name, product10.category, product10.price],
+            [product11.name, product11.category, product11.price], [product12.name, product12.category, product12.price],
+            [product13.name, product13.category, product13.price], [product14.name, product14.category, product14.price],
+            [product15.name, product15.category, product15.price], [product16.name, product16.category, product16.price],
+            [product17.name, product17.category, product17.price], [product18.name, product18.category, product18.price],
+            [product19.name, product19.category, product19.price], [product20.name, product20.category, product20.price],
+            [product21.name, product21.category, product21.price], [product22.name, product22.category, product22.price]
+        ]
+
          // filter products by category
-         category = "pant"
+         category = "couch"
          lst = productsList.filter(function(x) {return x[1].includes(category)})
  
          homePage.filterProducts(category)
@@ -313,7 +364,7 @@ describe('Sort-and-Filter', () => {
          homePage.sortProducts('loHi')
          lst = lst.sort(function(a, b){ 
              if (a[2] === b[2]) {
-                 return a[0].localeCompare(b[0])
+                 return b[0].localeCompare(a[0])
              } return a[2] - b[2]             
          }) // sort products list
  
