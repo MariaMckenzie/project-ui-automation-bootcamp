b43f8c6edf18fa8ef8cadbc573a00ef10d1ebaf8
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-12-15 13:09:31 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -16,8 +16,13 @@ module.exports = defineConfig({
             html: false,
             json: true
         },
-    },
-    env: {
-        CYPRESS_MAILSLURP_API_KEY: '9fcabbf7701d5e08fe4dcb65559a44ab360bbd5d70fcb6ab77b69f9626db41ca',
-    }
+
+        defaultCommandTimeout: 30000,
+        responseTimeout: 30000,
+        requestTimeout: 30000,
+        
+        env: {
+            CYPRESS_MAILSLURP_API_KEY: '9fcabbf7701d5e08fe4dcb65559a44ab360bbd5d70fcb6ab77b69f9626db41ca',
+        },
+    },      
 });
