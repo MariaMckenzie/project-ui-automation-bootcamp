28fb8141da9f49989d4ca8976d98fb6f85a1e07b
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-12-15 16:16:15 -0500

    Update data file

--- cypress/e2e/data/data-driven/user.data.js
+++ cypress/e2e/data/data-driven/user.data.js
@@ -1,36 +1,38 @@
-module.exports = {
-    newUser: {
-        email: "johndoe5@mail.com",
-        password: "P@ssword1"
-    },
-    existingUser: {
+module.exports = [
+    {
+        userType: "an existing user",
         email: "johndoe1@mail.com",
         password: "P@ssword1",
         errorMsg: "We're sorry, something went wrong"
     },
-    nonexistingUser: {
+    {
+        userType: "a user that does not exist",
         email: "jd15@mail.com",
         password: "P@ssword1",
         errorMsg: "Wrong email or password"
     },
-    invalidData: {
+    {
+        userType: "a user whose email and password are not valid",
         email: "janedoe",
         password: "password",
         errorMsg: ["Email is invalid", "Password is invalid"]
     },
-    missingEmail: {
+    {
+        userType: "a user without an email",
         email: "",
         password: "P@ssword1",
         errorMsg: "Email can't be blank"
     },
-    missingPassword: {
+    {
+        userType: "a user without a password",
         email: "janedoe@mail.com",
         password: "",
         errorMsg: "Password can't be blank"
     },
-    missingData: {
+    {
+        userType: "user who leaves both fields blank",
         email: "",
         password: "",
         errorMsg: ["Email can't be blank", "Password can't be blank"]
     }
-}
+]
